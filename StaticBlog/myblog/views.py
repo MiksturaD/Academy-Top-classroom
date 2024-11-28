8cfdefa98f62c620a058030b2794fe03227746cf
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2024-11-28 17:18:22 +0700

    GP_My_blog_Static

---
+++ StaticBlog/myblog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def main(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contacts(request):
+    return render(request, 'contacts.html')
